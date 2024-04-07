1d64ff3978c115a7a3edd4a3d299012b18afdf65
Pomalec <spoma2002@gmail.com>
2024-04-07 14:23:32 +0100

    copies of room with tilesets

--- Assets/Script/Interactables/InteractableObject.cs
+++ Assets/Script/Interactables/InteractableObject.cs
@@ -9,21 +9,48 @@ public class InteractbleObject : MonoBehaviour
     [SerializeField] private Dialog _dialog;
     [SerializeField] private bool _startsDialog = true;
     [SerializeField] private bool minigame;
+    [SerializeField] private bool book;
+    [SerializeField] private Dialog bookb1;
+    [SerializeField] private Dialog bookb2;
+    [SerializeField] private Dialog bookb3;
+    [SerializeField] private Dialog bookb4;
+    [SerializeField] private Dialog bookb5;
     public void TryInteract()
     {
-        if (minigame)
+        if (book)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(2);
-            return;
-        }
+            int booktype = this.gameObject.GetComponent<bookpuzzle>().gettype();
+            switch (booktype)
+            {
+                case 0: DialogManager.Instance.Show(_dialog); break;
+                case 1: DialogManager.Instance.Show(bookb1); break;
+                case 2: DialogManager.Instance.Show(bookb2); break;
+                case 3: DialogManager.Instance.Show(bookb3); break;
+                case 4: DialogManager.Instance.Show(bookb4); break;
+                case 5: DialogManager.Instance.Show(bookb5); break;
+            }
 
-        if (!isInteractable || !_startsDialog) return;
-        DialogManager.Instance.Show(_dialog);
-        //Maybe destroy after dialog?
-        if (canPickup)
+        }
+        else
+
+
+
+
         {
-            AudioManager.Instance.Play(Echo.Audio.AudioType.Pickup);
-            Destroy(gameObject);
+            if (minigame)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+                return;
+            }
+
+            if (!isInteractable || !_startsDialog) return;
+            DialogManager.Instance.Show(_dialog);
+            //Maybe destroy after dialog?
+            if (canPickup)
+            {
+               AudioManager.Instance.Play(Echo.Audio.AudioType.Pickup);
+                Destroy(gameObject);
+            }
         }
     }
 }
